518c7867b2eb50bdeae56ea62554d973c0c2237e
jianzhou <jianzhou@eef5ea85-cc2e-0410-bd84-0fae1f012da8>
2010-08-25 13:00:02 +0000

    Added an ouput to validate. To validate the equilavence between input and output, we need to at least do the following extension for this TV.   1) let TV consider sstate.smap as a set -- to validate instruction scheduling   2) only checking an abservable set of variables -- to validate dead exp elimination, deal with additional temporal variables   3) adding eq laws for arithmetic, memory, cast or other insns -- to validate constant prop, copy prop...   The current TV can only validate identical programs because SSA forms have already considered variables 'copy propagations'. To support Softbound, we need to do 3) for cast insns, say bitcast does not change the value from src to target, and 2) for new temporal variables in ouputs.
    git-svn-id: https://webdav.seas.upenn.edu/svn/sol/vol@2579 eef5ea85-cc2e-0410-bd84-0fae1f012da8

--- src/Parser/Output.ll
+++ src/Parser/Output.ll
@@ -1,4 +1,4 @@
-; ModuleID = 'Input.bc'
+; ModuleID = 'Output.bc'
 target datalayout = "e-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-a0:0:64-s0:64:64-f80:128:128"
 target triple = "x86_64-unknown-linux-gnu"
 
@@ -35,8 +35,8 @@ Loop:                                             ; preds = %Loop, %entry
   %nextindvar = add i32 %indvar, 1                ; <i32> [#uses=1]
   %5 = call i99 @ttt(i32* %0)                     ; <i99> [#uses=0]
   %6 = icmp eq i32 4, 5                           ; <i1> [#uses=0]
-  %x1 = add i13 7, 8                              ; <i13> [#uses=0]
   %x2 = load i32* %0, align 4                     ; <i32> [#uses=0]
+  %x1 = add i13 7, 8                              ; <i13> [#uses=0]
   br i1 false, label %Loop, label %return
 
 return:                                           ; preds = %Loop
--- src/Parser/main.ml
+++ src/Parser/main.ml
@@ -3,24 +3,32 @@ open Llvm
 open Symexe
 
 let main in_filename out_filename =
-	let c = create_context () in
-	let mbuf = MemoryBuffer.of_file in_filename in
-	let m = Llvm_bitreader.parse_bitcode c mbuf in
-	let st = SlotTracker.create_of_module m in
+	let ic = create_context () in
+	let imbuf = MemoryBuffer.of_file in_filename in
+	let im = Llvm_bitreader.parse_bitcode ic imbuf in
+	let ist = SlotTracker.create_of_module im in
 	
-	(* I use prerr but not print, because dump_ outputs to stderr. *)
-	dump_module m;
-	
-	Llvm_pretty_printer.travel_module st m;
-	
-	let coqm = Translator.translate_module st m in 	
-	Coq_pretty_printer.travel_module coqm;	
-	eprintf "TV=%b\n" (tv_module coqm coqm);
+	dump_module im;
+	Llvm_pretty_printer.travel_module ist im;
+	let coqim = Translator.translate_module ist im in
+	Coq_pretty_printer.travel_module coqim;
+
+	let oc = create_context () in
+	let ombuf = MemoryBuffer.of_file out_filename in
+	let om = Llvm_bitreader.parse_bitcode oc ombuf in
+	let ost = SlotTracker.create_of_module om in
+
+	let coqom = Translator.translate_module ost om in
+			
+	eprintf "TV=%b\n" (tv_module coqim coqom);
 	
 	(* write the module to a file *)
-	if not (Llvm_bitwriter.write_bitcode_file m out_filename) then exit 1;
-	SlotTracker.dispose st;
-	dispose_module m
+	(* if not (Llvm_bitwriter.write_bitcode_file m out_filename) then exit 1; *)
+	
+	SlotTracker.dispose ist;
+	SlotTracker.dispose ost;
+	dispose_module im;
+	dispose_module om
 
 let () = match Sys.argv with
 	| [| _; in_filename; out_filename |] -> main in_filename out_filename
