939439b5685e1ca938d4af419367726998afa94f
jianzhou <jianzhou@eef5ea85-cc2e-0410-bd84-0fae1f012da8>
2011-04-04 14:02:46 +0000

    changed testcases locations and names.
    git-svn-id: https://webdav.seas.upenn.edu/svn/sol/vol@2822 eef5ea85-cc2e-0410-bd84-0fae1f012da8

--- tests/free/testb.c
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void test(int mm)
-{
-  int arr1;
-  free (&arr1);
-}
--- tests/simple/testb.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-void test(int mm)
-{
-  int* i=(int*)malloc(sizeof(int)*10);
-  i[mm] = 42;
-  printf("%d\n", *i);
-}
